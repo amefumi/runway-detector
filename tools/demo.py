6d546461ed9a60401774bc2cc87edd2ef0949f33
Bruce <zty326@hotmail.com>
2021-12-27 01:19:24 +0800

    Modify match_thres in assignment

--- tools/demo.py
+++ tools/demo.py
@@ -88,7 +88,7 @@ def make_parser():
     # tracking args
     parser.add_argument("--track_thresh", type=float, default=0.5, help="tracking confidence threshold")
     parser.add_argument("--track_buffer", type=int, default=30, help="the frames for keep lost tracks")
-    parser.add_argument("--match_thresh", type=float, default=0.6, help="matching threshold for tracking")
+    parser.add_argument("--match_thresh", type=float, default=0.8, help="matching threshold for tracking")
     parser.add_argument('--min_box_area', type=float, default=4, help='filter out tiny boxes')
     parser.add_argument("--mot20", dest="mot20", default=False, action="store_true", help="test mot20.")  # TODO: 去掉无用的parser
     return parser
